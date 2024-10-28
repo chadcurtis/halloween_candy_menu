b9d0bd87fe2054eb6ad8ea6d431fc27f498a026b
Chad Curtis <chad@chadwick.site>
2024-10-28 17:50:35 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+//import Image from "next/image";
+const arrayData = [ "blah", "bleh", "bluh"];
+
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+         <div className="FlexGrid">
+             <div className="row">
+      {arrayData.map(item =>(
+        <div key={item}>
+          {item}
+          <p>{item}</p>
+        </div>
+      ))
+
+     }
+         </div>
+         </div>
+      </main>
+    </div>
+  );
+}
