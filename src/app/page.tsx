f4b471d31f7e6623794e1eb626e87a4e51ba11e7
Chad Curtis <chad@chadwick.site>
2024-10-28 18:56:52 -0500

    static data render + gitignore update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
-//import Image from "next/image";
-const arrayData = [ "blah", "bleh", "bluh"];
-
+import Image from "next/image";
+const candyList = [
+  {
+    name: "Chocolate Bar",
+    image: "/images/Chocolate Bar.jpg",
+  },
+  {
+    name: "Nutty Bar",
+    image: "/images/Nutty Bar.jpg",
+  },
+  {
+    name: "Fruit Bar",
+    image: "/images/Fruit Bar.jpg",
+  },
+  {
+    name: "Toothbrush",
+    image: "/images/Toothbrush.jpg",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-         <div className="FlexGrid">
-             <div className="row">
-      {arrayData.map(item =>(
-        <div key={item}>
-          {item}
-          <p>{item}</p>
+    <div className="grid grid-cols-3 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {candyList.map((item) => (
+        <div
+          className="p-16 outline outline-5 rounded outline-red-300 outline-offset-4 border border-5 border-red-300 row"
+          key={item.name}
+        >
+          <Image src={item.image} alt={item.name} width={250} height={250} />
+          <p>{item.name}</p>
         </div>
-      ))
-
-     }
-         </div>
-         </div>
-      </main>
+      ))}
     </div>
   );
 }
