cda782353007d0a5764fc6e4176b34642cc623e6
Chad Curtis <chad@chadwick.site>
2024-10-28 19:13:18 -0500

    Clean up template somewhat - base layout complete.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,11 @@ export default function Home() {
     <div className="grid grid-cols-3 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {candyList.map((item) => (
         <div
-          className="p-16 outline outline-5 rounded outline-red-300 outline-offset-4 border border-5 border-red-300 row"
+          className="p-10 outline outline-5 rounded outline-red-300 outline-offset-4 border border-5 border-red-300 row"
           key={item.name}
         >
-          <Image src={item.image} alt={item.name} width={250} height={250} />
-          <p>{item.name}</p>
+          <Image src={item.image} alt={item.name} width={400} height={400} />
+          <div className="center pt-10">{item.name}</div>
         </div>
       ))}
     </div>
