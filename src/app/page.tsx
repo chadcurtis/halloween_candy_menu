c7e2f6d5bf930b077c27daadd88a0636ce5c5dca
Chad Curtis <chad@chadwick.site>
2024-10-30 20:10:57 -0500

    clean up UI concept, attempt to add smoke effects and animations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,79 @@
 import Image from "next/image";
+import "animate.css";
+import { SmokeScene } from "react-smoke";
+import { useMemo } from "react";
+import * as THREE from "three";
+
+// Images are stored under ../../public/images/
 const candyList = [
   {
-    name: "Chocolate Bar",
-    image: "/images/Chocolate Bar.jpg",
+    name: "Hershey's Chocolate",
+    image: "/images/hersheys_bar.png",
   },
   {
-    name: "Nutty Bar",
-    image: "/images/Nutty Bar.jpg",
+    name: "Hershey's with Almonds",
+    image: "/images/hersheys_almond_chocolate_bar.png",
   },
   {
-    name: "Fruit Bar",
-    image: "/images/Fruit Bar.jpg",
+    name: "KitKat",
+    image: "/images/kitkat.png",
   },
   {
-    name: "Toothbrush",
-    image: "/images/Toothbrush.jpg",
+    name: "Milkyway",
+    image: "/images/milkyway.png",
+  },
+  {
+    name: "M&M's",
+    image: "/images/mm.png",
+  },
+  {
+    name: "M&M's Peanuts",
+    image: "/images/mm_peanut.png",
+  },
+  {
+    name: "Reese's Cup",
+    image: "/images/reeses.png",
+  },
+  {
+    name: "Snickers",
+    image: "/images/snickers.png",
+  },
+  {
+    name: "Twix",
+    image: "/images/twix.png",
   },
 ];
 
 export default function Home() {
+  const smokeColor = useMemo(() => new THREE.Color("red"), []);
   return (
-    <div className="grid grid-cols-3 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {candyList.map((item) => (
-        <div
-          className="p-10 outline outline-5 rounded outline-red-300 outline-offset-4 border border-5 border-red-300 row"
-          key={item.name}
-        >
-          <Image src={item.image} alt={item.name} width={400} height={400} />
-          <div className="center pt-10">{item.name}</div>
-        </div>
-      ))}
-    </div>
+    <main className="bg-slate-900">
+      <SmokeScene
+        smoke={{
+          color: smokeColor,
+          density: 50,
+          enableRotation: true,
+        }}
+      />
+      <div className="grid grid-cols-3 items-center justify-items-center h-screen p-8 pb-10 gap-5 sm:p-15 font-[family-name:var(--font-geist-sans)]">
+        {candyList.map((item) => (
+          <div
+            className="w-1/2 p-10 outline outline-4 rounded outline-orange-400 outline-offset-4 border border-5 border-orange-300 bg-gray-700 row"
+            key={item.name}
+          >
+            <Image
+              className="animate__animated animate__pulse animate__infinite animate__slow"
+              src={item.image}
+              alt={item.name}
+              width={200}
+              height={100}
+            />
+            <div className="text-center pt-2 font-bold tracking-wide text-slate-100">
+              {item.name}
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
